fac9a0152f96a8cd949410e051b200cdcdd87175
SPEED OF LIGHT <mac@MacBookPro.lan>
2026-01-13 04:53:23 -0800

    add loading and error pages to public routes, change db port cause of MacOS

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -25,6 +25,8 @@ app.use(
       "https://spreadlovenetwork.com",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], // Ad
   })
 );
 app.use(express.json());
